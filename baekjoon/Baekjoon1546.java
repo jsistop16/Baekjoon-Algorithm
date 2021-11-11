a84bafb6015083f8a93241a11fbb26e7009389df
jsistop16 <83804417+jsistop16@users.noreply.github.com>
2021-11-11 23:42:15 +0900

    Add files via upload

---
+++ baekjoon/Baekjoon1546.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.*;
+
+public class Baekjoon1546 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		double[] arr = new double[N];
+		
+		for(int i=0; i<N; i++) {
+			arr[i]=sc.nextDouble();
+		}
+		sc.close();
+		Arrays.sort(arr);
+		double sum=0;
+		for(int i=0; i<N; i++) {
+			sum+=(arr[i]/arr[N-1])*100;
+		}
+		System.out.println(sum/N);
+	}
+
+}
