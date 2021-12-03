38b150b87b091d96af8a5b99a9c7e074487706fc
jsistop16 <83804417+jsistop16@users.noreply.github.com>
2021-12-03 17:51:54 +0900

    Stack사용( 괄호 2가지 대칭 여부 )

---
+++ baekjoon/Baekjoon4949.java
@@ -0,0 +1,62 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Baekjoon4949 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			String str = br.readLine();
+			
+			if(str.equals(".")) {
+				break;
+			}
+			sb.append(solve(str)+"\n");
+			
+		}
+		System.out.println(sb);
+	}
+	
+	public static String solve(String str) {
+		Stack<Character> stack = new Stack<>();
+		
+		for(int i=0; i<str.length(); i++) {
+			char c = str.charAt(i);
+			
+			if(c=='(' || c=='[') {
+				stack.push(c);
+			}
+			
+			else if(c==')') {
+				if(stack.empty() || stack.peek()!='(') {
+					return "no";
+				}
+				else {
+					stack.pop();
+				}
+			}
+			
+			else if(c==']') {
+				if(stack.empty() || stack.peek() != '[') {
+					return "no";
+				}
+				else {
+					stack.pop();
+				}
+			}
+			
+		}
+		if(stack.empty()) {
+			return "yes";
+		}
+		else {
+			return "no";
+		}
+	}
+
+}
