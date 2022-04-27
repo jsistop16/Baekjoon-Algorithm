cf5f122231cc0d745ef7f41cd9329f22c820d694
jsistop16 <83804417+jsistop16@users.noreply.github.com>
2022-04-27 20:33:22 +0900

    알고리즘, System.exit(0);

--- baekjoon/Baekjoon2309.java
+++ baekjoon/Baekjoon2309.java
@@ -4,23 +4,42 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Baekjoon2309 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int[] arr = new int[9];
-		
+		int sum = 0;
 		for(int i=0; i<9; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
+			sum+=arr[i];
 		}//9명의 난쟁이 키 입력 완료
-		List<Integer> list = Arrays.asList(arr);
 		
-		Collections.shuffle(list);
+		Arrays.sort(arr);
 		
-		System.out.println(list);
+		for(int i=0; i<arr.length-1; i++) {
+			for(int j=i+1; j<arr.length; j++) {
+				if(sum - arr[i] - arr[j] == 100) {
+					for(int k=0; k<arr.length; k++) {
+						if(k == i || k == j) {
+							continue;
+						}
+						System.out.println(arr[k]);	
+					}
+				System.exit(0);
+				}
+			}
+		}
+		
+		/*
+		 * List<Integer> list = Arrays.asList(arr);
+		 * 
+		 * Collections.shuffle(list);
+		 * 
+		 * System.out.println(list);
+		 */
+		
 	}
 
 }
